ce987f418c4bf5a6ae777cf7522a8b1f61e0e662
Gaurav Agrawal <gauravag.1005@gmail.com>
2023-02-27 19:33:35 +0530

    deps: installing Tailwind CSS with Flowbite
    ‣ https://flowbite.com/docs/getting-started/react/

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { Footer } from "flowbite-react";
+
 export default function App() {
-  return (
-    <>
-    </>
-  );
+	return (
+		<>
+			<Footer container={true}>
+				<Footer.Brand href="." name="Admin UI" />
+				<Footer.Copyright href="https://github.com/gauravagrwal/Admin-UI" by="Gaurav Agrawal" year={2023} />
+			</Footer>
+		</>
+	);
 }
