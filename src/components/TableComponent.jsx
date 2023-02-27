aeebfcf323852451c38e6f4bb669d664b12e387f
Gaurav Agrawal <gauravag.1005@gmail.com>
2023-02-27 20:42:58 +0530

    feat: adding Search functionality

--- src/components/TableComponent.jsx
+++ src/components/TableComponent.jsx
@@ -1,10 +1,35 @@
-import { Table } from "flowbite-react";
+import { Table, TextInput } from "flowbite-react";
+import { useState } from "react";
 
 import TableRowComponent from "./TableRowComponent";
 
 export default function TableComponent({ tData }) {
+    //#region Search
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+
+    function handleSearch(e) {
+        setSearchTerm(e.target.value);
+        if (searchTerm === "")
+            setSearchResults(tData);
+        else {
+            const filteredData = tData.filter((data) => {
+                if (
+                    data.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                    data.email.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                    data.role.toLowerCase().includes(searchTerm.toLowerCase())
+                )
+                    return data;
+            });
+            setSearchResults(filteredData);
+        }
+    }
+    //#endregion
+
     return (
         <>
+
+            <TextInput addon="🔍" placeholder="Search by name, email or role" value={searchTerm} onChange={handleSearch} />
 
             <Table hoverable={true}>
 
@@ -14,11 +39,22 @@ export default function TableComponent({ tData }) {
                     <Table.HeadCell>Role</Table.HeadCell>
                 </Table.Head>
 
-                <Table.Body className="divide-y">
-                    {tData.map((row) => (
-                        <TableRowComponent key={row.id} row={row} />
-                    ))}
-                </Table.Body>
+                {searchResults.length === 0
+                    ? (
+                        <Table.Body>
+                            <Table.Row>
+                                <Table.Cell colSpan={3}>No Records Found!</Table.Cell>
+                            </Table.Row>
+                        </Table.Body>
+                    )
+                    : (
+                        <Table.Body className="divide-y">
+                            {searchResults.map((row) => (
+                                <TableRowComponent key={row.id} row={row} />
+                            ))}
+                        </Table.Body>
+                    )
+                }
 
             </Table>
 
