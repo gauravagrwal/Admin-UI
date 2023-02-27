615bdc8e682aa1b08a82c2c8791c39f338039656
Gaurav Agrawal <gauravag.1005@gmail.com>
2023-02-27 19:53:21 +0530

    feat: (R) fetching data from endpoint using Axios
    ‣ https://axios-http.com/docs/intro

---
+++ src/AdminUI.jsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function AdminUI() {
+	const [data, setData] = useState([]);
+
+	async function getData() {
+		try {
+			const response = await axios.get("https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json");
+			console.info(response);
+			setData(response.data);
+		}
+		catch (error) {
+			console.error(error);
+		}
+	}
+
+	useEffect(() => {
+		getData();
+	}, []);
+
+	return (
+		<></>
+	);
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { Footer } from "flowbite-react";
 
+import AdminUI from "./AdminUI";
+
 export default function App() {
 	return (
 		<>
+			<main>
+				<AdminUI />
+			</main>
+
 			<Footer container={true}>
 				<Footer.Brand href="." name="Admin UI" />
 				<Footer.Copyright href="https://github.com/gauravagrwal/Admin-UI" by="Gaurav Agrawal" year={2023} />
